6cf682740bb3645f2f356f9234a975673b4702ec
Mohammed-Sunasra <Mohammed-Sunasra@users.noreply.github.com>
2017-08-14 19:46:03 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,14 @@
 def solution(list_of_nums):
-    """Enter Code Here"""
+    dic = dict()
+    count_even,count_odd = 0, 0
+    for i in list_of_nums:
+        if i % 2 == 0:
+            count_even += 1
+        else:
+            count_odd += 1
+    dic['ODD'] = count_odd
+    dic['EVEN'] = count_even
+    return dic
+
+l = [1, 2, 3, 5, 8, 9]
+print solution(l)
